9235ca6da34637fda5ca3d9e782a39d441684473
mcamediacreative-creator <mcamediacreative@gmail.com>
2026-06-28 12:56:57 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.INVOICE_DESK_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAW214vxvxAKHkMCORcJw4tYxySkcCZjmU",
-  authDomain: "invoice-desk-pwa.firebaseapp.com",
-  projectId: "invoice-desk-pwa",
-  storageBucket: "invoice-desk-pwa.firebasestorage.app",
-  messagingSenderId: "857125056436",
-  appId: "1:857125056436:web:29b3dfd3dea39ce4d38ce1"
-};
